692c3a0bdfef61d6762983c2836781d9d538a7fa
David Dellsperger <david.dellsperger@gmail.com>
2021-12-04 23:55:27 -0600

    Generalize Point since Java 11 doesn't have one

--- src/main/java/net/ddellspe/day05/Day05.java
+++ src/main/java/net/ddellspe/day05/Day05.java
@@ -8,38 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
-class Point {
-  int x;
-  int y;
-
-  public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Point point = (Point) o;
-    return x == point.x && y == point.y;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-
-  @Override
-  public String toString() {
-    return "Point [x=" + x + ", y=" + y + "]";
-  }
-}
+import net.ddellspe.utils.Point;
 
 public class Day05 {
   public static List<String> readInData(String filename) {
